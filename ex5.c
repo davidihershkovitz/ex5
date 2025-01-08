5f40847d24ebb3d87dd63494cc623302e1d368e3
davidi <davidi28her@gmail.com>
2025-01-08 19:17:24 +0200

    16 version

--- ex5.c
+++ ex5.c
@@ -36,27 +36,35 @@ void removePlaylist(Playlist*** playlists, int* playlistsNum);
 
 
 char* readingInput() {
-    char* input = malloc(sizeof(char));
-    if (!input) exit(1); // Exit if memory allocation fails
-
-    int count = 0; // Keeps track of the number of characters
-    char c; // Character to be read
-
-    while ((c = getchar()) != '\n' && c != '\r' && c != EOF) { // Stop on newline (\n), carriage return (\r), or EOF
-        input = realloc(input, (count + 2) * sizeof(char)); // Allocate memory for the next character
-        if (!input) exit(1); // Exit if memory allocation fails
-        input[count++] = c; // Add the character to the string
+    char* input = malloc(sizeof(char)); // Allocate initial memory
+    if (!input) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
 
-    // Consume the second part of "\r\n" if present (e.g., Windows-style line endings)
-    if (c == '\r' && (c = getchar()) != '\n' && c != EOF) {
-        ungetc(c, stdin); // Push back the extra character to the input buffer
+    int countLetter = 0;
+    char letter;
+
+    while ((letter = getchar()) != '\n' && letter != '\r' && letter != EOF) {
+        input = realloc(input, (countLetter + 2) * sizeof(char)); // Resize memory
+        if (!input) {
+            printf("Memory allocation failed\n");
+            exit(1);
+        }
+        input[countLetter++] = letter; // Store the character
     }
 
-    input[count] = '\0'; // Null-terminate the string
+    // If '\r' is found, check for '\n' (handle Windows-style input)
+    if (letter == '\r') {
+        char nextChar = getchar();
+        if (nextChar != '\n' && nextChar != EOF) {
+            ungetc(nextChar, stdin); // Push back the extra character if it's not '\n'
+        }
+    }
+
+    input[countLetter] = '\0'; // Null-terminate the string
     return input;
 }
-
 // Function to add a playlist
 void addPlaylist(Playlist*** playlists, int* playlistsNum) {
     char* playlistName = readingInput(); // Prompt already happens here
